75464aa0ab1eebe5601044c6ac7d805b668431c1
mafahir <mafahir@pavertech.my>
2020-04-11 12:20:26 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TabbarComponent } from './tabbar/tabbar.component';
 import { IndexComponent } from './home/index/index.component';
+import { HomeComponent } from './home/home/home.component';
+import { PromoComponent } from './promo/promo/promo.component';
+import { ActivityComponent } from './activity/activity/activity.component';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { NewsComponent } from './news/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabbarComponent,
-    IndexComponent
+    IndexComponent,
+    HomeComponent,
+    PromoComponent,
+    ActivityComponent,
+    ProfileComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
