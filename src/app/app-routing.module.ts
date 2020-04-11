00c6606ea4a6ea52df9614c57cf08beafa25641c
mafahir <mafahir@pavertech.my>
2020-04-11 12:23:40 +0800

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { PromoComponent } from './promo/promo/promo.component';
+import { ActivityComponent } from './activity/activity/activity.component';
+import { NewsComponent } from './news/news/news.component';
 
 const routes: Routes = [
-
+  {path: 'promo', component: PromoComponent},
+  {path: 'activity', component: ActivityComponent},
+  {path: 'home', component: ActivityComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'profile', component: PromoComponent}
 ];
 
 @NgModule({
