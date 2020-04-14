91d8909868b99ac13c665f572859bf5c302b27f4
mafahir <mafahir@pavertech.my>
2020-04-15 02:33:56 +0800

    commit

--- src/app/home/payment/productinamount/productinamount.component.ts
+++ src/app/home/payment/productinamount/productinamount.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmpaymentComponent } from '../confirmpayment/confirmpayment.component';
 
 @Component({
   selector: 'app-productinamount',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductinamountComponent implements OnInit {
 
-  constructor() { }
+   
+  public isActive = true;
+  public productMenu = '';
+  public productName = '';
+  public productAmount = '';
+  public productAccount = '';
+
+  constructor(private route: ActivatedRoute, private router: Router,private _productService: ProductService, private snackBar: MatSnackBar, 
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    // let menu = this.route.snapshot.paramMap.get('menu');
+    // let productName = this.route.snapshot.paramMap.get('product');
+    
+    this.productMenu = this._productService.getMenu();
+    this.productName = this._productService.getProductName();
   }
+
+    
+  btnConfirmPayment(){
+    if (this.productAmount != '' && this.productAccount != '') {
+    // if (true) {
+
+      let dialogRef = this.dialog.open(ConfirmpaymentComponent, {data: {name: this.productName, amount: this.productAmount, account: this.productAccount}});
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('Dialog result: '+ result);
+      });
+
+    } else {
+      let message = '';
+      if (this.productAccount == '') {
+        message += ' enter account,';
+      } 
+      if (this.productAmount == '') {
+        message += 'enter amount';
+      } 
+      
+      this.snackBar.open(message, 'Dismiss', {duration: 3000});
+      
+    }
+    
+
+    // this.router.navigate(['/home/'+this.menu, this.productName]);
+  }
+
+  
+  account(event: any) { // without type info
+    this.productAccount = event.target.value;
+    this._productService.setAccountNumber(event.target.value);
+    // console.log(this.productAccount);
+  }
+
+  
+  amount(event: any) { // without type info
+    this.productAmount = event.target.value;
+    this._productService.setAccountNumber(event.target.value);
+    // console.log(this.productAccount);
+  }
+  
 
 }
